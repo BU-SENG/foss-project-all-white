b4d7a017e9571a4bc97f6296becb0ada82b94aa0
Abasiekeme <abasiekeme2022@gmail.com>
2025-11-19 23:15:32 +0100

    Encountered an error so tried solving it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
-// tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
       extend: {
         colors: {
-          background: '#0B1410', // Deep dark green background
-          surface: '#13231B',    // Slightly lighter for cards
-          primary: '#00E359',    // Neon green buttons
-          secondary: '#1F3328',  // Dark green tags
+          background: '#0B1410',
+          surface: '#13231B',
+          primary: '#00E359',
+          secondary: '#1F3328',
           textMain: '#FFFFFF',
           textMuted: '#8E9B93',
         }
-      }
-    }
+      },
+    },
+    plugins: [],
   }
