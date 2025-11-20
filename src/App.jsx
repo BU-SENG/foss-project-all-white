7423e09d1d72948db314bbbcfd1e2725f97274c1
Abasiekeme <abasiekeme2022@gmail.com>
2025-11-20 02:07:48 +0100

    Updated App.jsx to contain the new change

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 
 import HomeScreen from './HomeScreen';
-// Removed SearchScreen import
 import CreateListingScreen from './CreateListingScreen';
 import ChatListScreen from './ChatListScreen';
+import ChatScreen from './ChatScreen'; // Import the new screen
 import MyListingsScreen from './MyListingsScreen';
 import ItemDetailScreen from './ItemDetailScreen';
 
@@ -13,13 +13,12 @@ function App() {
   return (
     <Router>
       <Navbar /> 
-      
       <div className="pt-0 md:pt-20"> 
         <Routes>
           <Route path="/" element={<HomeScreen />} />
-          {/* Removed Search Route */}
           <Route path="/create" element={<CreateListingScreen />} />
           <Route path="/chat" element={<ChatListScreen />} />
+          <Route path="/chat/:id" element={<ChatScreen />} /> {/* New Dynamic Route */}
           <Route path="/profile" element={<MyListingsScreen />} />
           <Route path="/item/:id" element={<ItemDetailScreen />} />
         </Routes>
